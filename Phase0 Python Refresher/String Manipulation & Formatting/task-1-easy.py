71d8804746ca6668ae016b8fc8d991c79c2cd4cd
Rithik Jaswal <jaswalrithik723@gmail.com>
2026-02-22 20:17:59 +0530

    Stting Manipulation & Formatting tasks

---
+++ Phase0 Python Refresher/String Manipulation & Formatting/task-1-easy.py
@@ -0,0 +1,5 @@
+# Task - Create two variables (action = "BLOCKED", ip = "192.168.1.50"). Use an f-string to print: "The action BLOCKED was taken on 192.168.1.50".
+
+action = "BLOCKED"
+ip = "192.168.1.50"
+print(f'The action {action} was taken on {ip}')
